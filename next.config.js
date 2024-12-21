e2cab8f5bc5118f5289019a27c9584bb838beb6e
learnershakil <learnershakil@gmail.com>
2024-12-21 21:18:37 +0530

    v0.0.5: fix minor issue

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-import micromatch from 'micromatch';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
@@ -9,12 +8,6 @@ const nextConfig = {
 			},
 		],
 	},
-	async rewrites() {
-		// Ensure patterns are valid and not overly complex
-		return [
-		  { source: '/valid-route', destination: '/api/valid' },
-		];
-	  },
 };
 
 export default nextConfig;
