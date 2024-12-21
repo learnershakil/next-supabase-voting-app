7d6af84dd6390408ba8adce6959258e94c345fbb
learnershakil <learnershakil@gmail.com>
2024-12-21 20:24:05 +0530

    v0.0.2: fix navbar

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
 	return (
 		<nav className=" w-full flex items-center justify-between ">
 			<Link href="/" className="flex items-center gap-2">
-				<h1 className="text-3xl font-bold">Learner's Arc </h1>
+				<h1 className="text-3xl font-bold">Learner&apos;s Arc </h1>
 
 				<RocketIcon className="w-5 h-5  animate-lanuch transition-all transform text-green-500" />
 			</Link>
